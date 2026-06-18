b153102088f38b8c232e4233cac634b8a0eb468d
xTidewaterx <79268288+xTidewaterx@users.noreply.github.com>
2026-06-18 20:49:19 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    domains: ["firebasestorage.googleapis.com", 'cdn.booniez.com', 'cdn.shopify.com', 'www.produits-scandinaves.com',
+     'th.bing.com', "en.visitbergen.com", 'skappeloslo.com','tellusdmsmedia.newmindmedia.com', 'no.daleofnorway.com','produits-scandinaves.com', 'pmstudio.com', 'www.piasweaters.com', "www.thenordicshop.net", "www.nya-evo.com", "eu.daleofnorway.com",'lh6.googleusercontent.com','produits-scandinaves.com', "opticsoutfitter.com", 'https://lh6.googleusercontent.com', 'files.stripe.com'
+], // ✅ Ensure Firebase domain is listed
+  },
 };
 
 export default nextConfig;
